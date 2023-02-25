b09603711f2b5058cae9b13c000c919ba1b6de2e
Ridwan Salim <ridwan.salim@gmail.com>
2023-02-25 22:32:03 +0600

    First

--- src/components/House.js
+++ src/components/House.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const House = () => {
-  return <div>House</div>;
+// import icons
+import {BiBed, BiBath, BiArea} from 'react-icons/bi'
+
+const House = ({house}) => {
+  const {image, type, country, address, bedrooms, bathrooms, surface, price} = house;
+
+  return (
+  <div>
+    <img src={image} alt="" />
+    <div>
+      <div>{type}</div>
+      <div>{country}</div>
+    </div>
+  </div>
+  );
 };
 
 export default House;
