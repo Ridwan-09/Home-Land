32e84266cda3f28e402457acb2e9e125aef2138e
Ridwan Salim <ridwan.salim@gmail.com>
2023-02-28 00:29:33 +0600

    First

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+// import data
+import {housesData} from '../data';
+
+// import use params
+import {useParams} from 'react-router-dom';
+
+// import icons
+import {BiBed, BiBath, BiArea} from 'react-icons/bi';
+
+// import link
+import {Link} from 'react-router-dom';
+// get the house id
+const {id} = useParams();
+
 const PropertyDetails = () => {
   return <div>PropertyDetails</div>;
 };
