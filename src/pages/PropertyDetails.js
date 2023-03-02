13d63d1dbf7c790ace1afae6b3f8bf1e192d7e15
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-02 11:13:44 +0600

    First

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -26,14 +26,21 @@ const house = housesData.find((house) => {
       <div className='container mx-auto min-h-[800px] mb-14'>
         <div className='flex flex-col lg:flex-row lg:items-center lg: justify-between'>
           <div>
-            <h2>{house.name}</h2>
-            <h3>{house.address}</h3>
+            <h2 className='text-2xl font-semibold'>{house.name}</h2>
+            <h3 className='text-lg mb-4'>{house.address}</h3>
           </div>
-          <div>
-            <div>{house.type}</div>
-            <div>{house.country}</div>
+          <div className='mb-4 lg:mb-0 flex gap-x-2 text-sm'>
+            <div className='bg-green-500 text-white px-3 rounded-full'>{house.type}</div>
+            <div className='bg-violet-500 text-white px-3 rounded-full'>{house.country}</div>
           </div>
-          <div>$ {house.price}</div>
+          <div className='text-3xl font-semibold text-violet-600'>$ {house.price}</div>
+        </div>
+        <div className='flex'>
+          <div className='max-w-[768px]'>
+            <div className='mb-8'>
+              <img src={house.imageLg} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
